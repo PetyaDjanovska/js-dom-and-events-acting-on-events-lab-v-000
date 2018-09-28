5d96b34f6c5871fc7efecfd8ad0476124497f7d9
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-28 22:38:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function addNewElementAsLi() {
   document.querySelector('input').value = '';
  }
 
- // function clearEmployeeListOnLinkClick() {
+ function clearEmployeeListOnLinkClick() {
  //   let link = document.querySelector('a[href='#']');
- // }
+ }
