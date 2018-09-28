a9de44e2ad21ef8ec2c89aacf97ea5ec26011df7
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-28 21:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,3 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
-
-function retrieveEmployeeInformation() {
-  return document.querySelector('input').value;
-}
-
-function addNewElementAsLi(name) {
-  let target = document.querySelector('.employee-list');
-  let li = document.createElement("li");
-  li.appendChild(document.createTextNode(${name}));
-  // target.appendChild(li);
-}
